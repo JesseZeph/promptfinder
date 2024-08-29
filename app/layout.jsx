757ddd86d43f5e4ec0a143e7df7cecb5b7a07b55
JesseZeph <jessezephyr@gmail.com>
2024-08-29 03:04:32 +0100

    force dynamic

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,31 @@
+export const dynamic = "force-dynamic";
 
-import '@styles/global.css';
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
+import "@styles/global.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Prompt Finder",
-    description: "Discover & Share AI Prompts"
-}
+  title: "Prompt Finder",
+  description: "Discover & Share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <Provider>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className="app">
-                <Nav/>
-                {children}
-            </main>
-            </Provider>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
